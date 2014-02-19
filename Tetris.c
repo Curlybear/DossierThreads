dbad0f21bc3c08acadcfbd98a6249b71c031b985
Curlybear <Tehcurlybear@gmail.com>
2014-02-19 11:15:04 +0100

    Création du threadDefileMessage

--- Tetris.c
+++ Tetris.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <signal.h>
 #include <time.h>
+#include <errno.h>
 #include "GrilleSDL.h"
 #include "Ressources.h"
 #include "ClientTetris.h"
@@ -18,22 +19,25 @@
 // Macros utlisees dans le tableau tab
 #define VIDE        0
 
+void setMessage(const char *texte);
+void* defileMessage(void*);
+
 int tab[NB_LIGNES][NB_COLONNES]
 ={ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
-  
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+
 typedef struct
 {
   int ligne;
@@ -55,47 +59,75 @@ PIECE pieces[7] = { 0,0,0,1,1,0,1,1,4,WAGNER,       // carre
                     0,0,0,1,0,2,1,1,4,CHARLET,      // T
                     0,0,0,1,0,2,0,3,4,MADANI };     // I
 
+char* message; // pointeur vers le message à faire défiler
+int tailleMessage; // longueur du message
+int indiceCourant; // indice du premier caractère à afficher dans la zone graphique
+
+pthread_mutex_t mutexMessage; // Mutex pour message,tailleMessage et indiceCourant
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc,char* argv[])
 {
+  pthread_mutex_init(&mutexMessage,NULL);
+
   EVENT_GRILLE_SDL event;
   char buffer[80];
   char ok;
- 
-  srand((unsigned)time(NULL));
+  pthread_t threadDefileMessage;
+
+  if((errno=pthread_create(&threadDefileMessage,NULL,defileMessage,NULL))!=0)
+  {
+    perror("Erreur de lancement de thread");
+  }
+
+
+srand((unsigned)time(NULL));
 
   // Ouverture de la grille de jeu (SDL)
-  printf("(THREAD MAIN) Ouverture de la grille de jeu\n");
-  fflush(stdout);
-  sprintf(buffer,"!!! TETRIS ZERO GRAVITY !!!");
-  if (OuvrirGrilleSDL(NB_LIGNES,NB_COLONNES,40,buffer) < 0)
-  {
+printf("(THREAD MAIN) Ouverture de la grille de jeu\n");
+fflush(stdout);
+sprintf(buffer,"!!! TETRIS ZERO GRAVITY !!!");
+if (OuvrirGrilleSDL(NB_LIGNES,NB_COLONNES,40,buffer) < 0)
+{
     printf("Erreur de OuvrirGrilleSDL\n");
     fflush(stdout);
     exit(1);
-  }
+}
 
   // Chargement des sprites et de l'image de fond
-  ChargementImages();
-  DessineSprite(12,11,VOYANT_VERT);
+ChargementImages();
+DessineSprite(12,11,VOYANT_VERT);
 
   // Exemple d'utilisation de GrilleSDL et Ressources --> code a supprimer
-  DessineLettre(10,12,'e');
-  DessineChiffre(10,14,4);
-  ok = 0;
-  while(!ok)
-  {
+DessineLettre(10,12,'e');
+DessineChiffre(10,14,4);
+ok = 0;
+while(!ok)
+{
     event = ReadEvent();
     if (event.type == CROIX) ok = 1;
     if (event.type == CLIC_GAUCHE) DessineSprite(event.ligne,event.colonne,WAGNER);
-  }
-
-  // Fermeture de la grille de jeu (SDL)
-  printf("(THREAD MAIN) Fermeture de la grille..."); fflush(stdout);
-  FermerGrilleSDL();
-  printf("OK\n"); fflush(stdout);
-
-  exit(0);
 }
 
+  // Fermeture de la grille de jeu (SDL)
+printf("(THREAD MAIN) Fermeture de la grille..."); fflush(stdout);
+FermerGrilleSDL();
+printf("OK\n"); fflush(stdout);
+
+exit(0);
+}
+
+void setMessage(const char *texte){
+
+}
+
+void* defileMessage(void*){
+    printf("(THREAD MESSAGE) Lancement du thread message\n");
+    pthread_mutex_lock(&mutexMessage);
+
+
+
+
+    pthread_mutex_unlock(&mutexMessage);
+}
