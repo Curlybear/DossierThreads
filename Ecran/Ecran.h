103907029ac917e5c5561e68271cb57b4fe3297a
bendem <online@bendem.be>
2014-02-21 00:38:57 +0100

    Reformatage du code du prof

--- Ecran/Ecran.h
+++ Ecran/Ecran.h
@@ -5,11 +5,11 @@
    Ecran.h
 */
 
-#ifndef        _ECRAN_H_
-#define        _ECRAN_H_
+#ifndef _ECRAN_H_
+#define _ECRAN_H_
 
-#define        NORM      0
-#define        GRAS      1
+#define NORM      0
+#define GRAS      1
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,15 +31,15 @@
 /* fonctions simplifiees de gestion d'ecran */
 
 void EffEcran(void);
-int LitChaine(char *,int,int,int,int);
-void AffChaine(char *,int,int,int);
+int LitChaine(char *, int, int, int, int);
+void AffChaine(char *, int, int, int);
 int Menu(void);
 void Pause(void);
-void Trace(char *,...);
-void Affiche(char *,...);
-void SortieErreur(char *,int);
+void Trace(char *, ...);
+void Affiche(char *, ...);
+void SortieErreur(char *, int);
 void SauveCurseur(void);
 void RestitueCurseur(void);
-void EffCadre(int ,int ,int ,int);
+void EffCadre(int, int, int, int);
 
 #endif     /* _ECRAN_H_ */
